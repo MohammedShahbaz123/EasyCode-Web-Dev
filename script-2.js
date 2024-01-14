ebb93b5789a716d2ee61fd074c10062710bc585c
Mohammed Shahaz <134310109+MohammedShahbaz123@users.noreply.github.com>
2024-01-14 12:47:27 +0530

    Add files via upload

---
+++ script-2.js
@@ -0,0 +1,27 @@
+let menu=document.querySelector('#menu');
+let navbar=document.querySelector('.navbar');
+let sections=document.querySelectorAll('.section');
+let navlink=document.querySelectorAll('header nav a');
+
+
+menu.onclick=()=>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll=()=>{
+    sections.forEach(sec=>{
+        let top=window.scrollY;
+        let offset=sec.offsetTop -150;
+        let height=sec.offsetHeight;
+        let id =sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlink.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+                navbar.classList.remove('active')
+            })
+        }
+    })
+}
